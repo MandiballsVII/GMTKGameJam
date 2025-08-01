62510743d2d4f05819ece762e0e83a95fd8912a3
MandiballsVII <149199591+MandiballsVII@users.noreply.github.com>
2025-08-01 22:20:23 +0200

    Animaciones player idle, correr y salto

--- Assets/Code/Player/PlayerAnimations.cs
+++ Assets/Code/Player/PlayerAnimations.cs
@@ -19,81 +19,52 @@ public class PlayerAnimations : MonoBehaviour
         
         animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
-        //hud.GetComponent<CanvasGroup>().alpha = 0f;
-        //showHud();
     }
 
     void Update()
     {
-        updateAnimationState();
+        UpdateAnimationState();
     }
 
-    private void updateAnimationState()
+    private void UpdateAnimationState()
     {
         MovementState state;
-        int idleNumber;
 
         if (playerMovement.dirX > 0f)
         {
-            //hideHud();
             state = MovementState.running;
-            if (transform.rotation == new Quaternion(0, -1, 0, 0))
-            {
-                playerLookingLeft = false;
-                transform.rotation *= Quaternion.Euler(0, 180, 0);
-                //transform.Rotate(0, 180, 0);
-            }            
-            //sprite.flipX = false;
-            idleNumber = 0;
-            animator.SetInteger("IdleNumber", idleNumber);
+            playerLookingLeft = false;
+            transform.localScale = new Vector3(1f, 1f, 1f); // mirar derecha
+
         }
         else if (playerMovement.dirX < 0f)
         {
-            //hideHud();
             state = MovementState.running;
-            if (transform.rotation == new Quaternion(0, 0, 0, 1))
-            {
-                playerLookingLeft = true;
-                transform.rotation *= Quaternion.Euler(0, 180, 0);
-                //transform.Rotate(0, 180, 0);
-            }
-            //sprite.flipX = true;
-            idleNumber = 0;
-            animator.SetInteger("IdleNumber", idleNumber);
+            playerLookingLeft = true;
+            transform.localScale = new Vector3(-1f, 1f, 1f); // mirar izquierda
         }
         else
         {
-            //showHud();
-            idleNumber = UnityEngine.Random.Range(0, 3);
-            animator.SetInteger("IdleNumber", idleNumber);
             state = MovementState.idle;
         }
 
         if (rb.velocity.y > 0.1f && !gameObject.GetComponent<PlayerJumpGlide>().isGrounded())
         {
-            //hideHud();
             animator.StopPlayback();
             state = MovementState.jumping;
-            idleNumber = 0;
-            animator.SetInteger("IdleNumber", idleNumber);
         }
 
         else if (rb.velocity.y < -0.1f)
-        {
-            //hideHud();
-            idleNumber = 0;
-            animator.SetInteger("IdleNumber", idleNumber);            
+        {          
             state = MovementState.falling; 
 
         }
         if(inDialog)
         {
             state = MovementState.idle;
-            idleNumber = 0;
-            animator.SetInteger("IdleNumber", idleNumber);
         }
         
-        animator.SetInteger("state", (int)state);
+        animator.SetInteger("State", (int)state);
     }
 
     
