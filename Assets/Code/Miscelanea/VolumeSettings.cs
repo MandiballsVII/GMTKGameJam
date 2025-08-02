23d801414a1baa5df827922de5c9c34ea7281824
ShibaInuEnjoyer <asiergaraupv@gmail.com>
2025-08-02 13:59:57 +0200

    Sliders MainMenu

---
+++ Assets/Code/Miscelanea/VolumeSettings.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
+
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        audioMixer.SetFloat("music", Mathf.Log10(volume)*20);
+    }
+    public void SetSFXVolume()
+    {
+        float volume = sfxSlider.value;
+        audioMixer.SetFloat("sfx", Mathf.Log10(volume) * 20);
+    }
+}
